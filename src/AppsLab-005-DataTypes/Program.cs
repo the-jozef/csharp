15d94505a0dde23ec0b031608fe29066206cf2a3
Liskoj25 <Liskoj25@Z00T1-08>
2025-09-10 13:12:34 +0200

    Changes12

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -6,7 +6,7 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            int myAge = 16;
+            /*int myAge = 16;
             Console.WriteLine(myAge);
 
             float pi = 3.14f;
@@ -21,7 +21,56 @@ namespace MyApp
             
             bool itis = true;
             Console.WriteLine(itis);
+            */
+
+           /* int a = 30;
+
+            int b = 5;
+
+            //scitavanie a + b alebo odcitavanie a - b
+            Console.WriteLine(a - b);
+
+            Console.WriteLine(a / b);
+
+            Console.WriteLine(a * b);
+
+            Console.WriteLine("volam sa izo");
+            */
+
+            int birthday = 13;
+            int month = 8;
+            int year = 2009;
+
+            int birthsum = birthday + month + year;
             
+            Console.WriteLine(birthsum);
+
+
+            int b = 10;
+
+            int l = birthsum * 10;
+            Console.WriteLine(l);
+
+            int m = birthsum / 10;
+            Console.WriteLine(m);
+
+            int g = birthday + month * year;
+            Console.WriteLine(g);
+            
+            Console.WriteLine(birthday + month * year);
+
+            string name = "jozeflisko";
+            Console.WriteLine(name + 2009);
+
+            Console.WriteLine(name + (birthday + month));
+
+            bool Result = 6 > 3;
+          
+            Console.WriteLine(Result);
+            bool resu = 10! > 5;
+            Console.WriteLine(resu);
+
+
 
 
 
