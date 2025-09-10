9dc79ad18f4891bad91596f5856b68569f670df9
Liskoj25 <Liskoj25@Z00T1-08>
2025-09-10 10:06:33 +0200

    Uloha na datatypes

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 16;
+            Console.WriteLine(myAge);
+
+            float pi = 3.14f;
+            Console.WriteLine(pi);
+
+            string myname = "jozeflisko";
+            Console.WriteLine(myname);
+
+
+            char gender = 'M';
+            Console.WriteLine(gender);
+            
+            bool itis = true;
+            Console.WriteLine(itis);
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
+
