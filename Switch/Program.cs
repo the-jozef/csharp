8c55683769f1ff751e7853a5efa108c00d2be9aa
Liskoj25 <Liskoj25@Z00T1-08>
2025-10-08 12:56:11 +0200

    switch

---
+++ Switch/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<int> list = new List<int>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                switch (command)
+                {
+                    case "add":
+                        {
+                            string cislotx = Console.ReadLine();
+                            int cislo = int.Parse(cislotx);
+                            list.Add(cislo);
+                            break;
+                        }
+                    case "end":
+                        {
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+
+                        }
+
+                        //doplnit ostane veci cez case 
+
+
+
+
+
+
+
+
+
+
+
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
