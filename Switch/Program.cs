5f47640d836f4ee927fc4aee5ba1c5c55d3632b8
Jozef Lisko <jozeflisko0@gmail.com>
2025-10-11 19:12:25 +0200

    switch

--- Switch/Program.cs
+++ Switch/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Switch
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Threading.Channels;
+
+namespace Switch
 {
     internal class Program
     {
@@ -6,70 +10,145 @@
         {
            List<int> list = new List<int>();
 
+            Console.WriteLine("add (pridat cislo)");
+            Console.WriteLine("del (zmazat cislo)");
+            Console.WriteLine("delat (zmazat konkretne cislo)");
+            Console.WriteLine("has (vypisat zoznam)");
+            Console.WriteLine("sort (vypise zoznam od najmensieho cisla)");
+            Console.WriteLine("end (ukoncit program)");
+            Console.WriteLine("count (napisat sucet vsetkych  cisiel)");
+            Console.WriteLine("avg (napisat priemer cisiel)");
+            Console.WriteLine("max (napisat najvecsie cislo)");
+            Console.WriteLine("min (napisat najmensie cislo)");
+            Console.WriteLine("get (napisat cislo na danom indexe)");
+            Console.WriteLine("help (vypise vsetky prikazy)");
+            Console.WriteLine("adds (pridanie viacej cisiel naraza le musi byt za nimi medzera)");
+
             while (true)
-            {
+             {
                 string command = Console.ReadLine();
                 switch (command)
                 {
                     case "add":
+                        {  
+                            string a = Console.ReadLine();
+                            int b = int.Parse(a);
+                            list.Add(b);
+                            break;
+                        }
+                    case "adds":
                         {
-                            string cislotx = Console.ReadLine();
-                            int cislo = int.Parse(cislotx);
-                            list.Add(cislo);
+                            string a = Console.ReadLine();
+                            string[] r = a.Split(' '); // rozdelí podľa medzery
+                            foreach (string c in r)
+                            {
+                                if (int.TryParse(c, out int cisla))     // bezpečné parsovanie
+                                
+                                    list.Add(cisla);
+                            }                    
+                            break;
+                        }                  
+                    case "del":
+                        {
+                            string a = Console.ReadLine();
+                            int b = int.Parse(a);
+                            list.Remove(b);
+                            break;
+                        }
+                    case "delat":
+                        { 
+                            string a = Console.ReadLine();
+                            int b = int.Parse(a);
+                            list.RemoveAt(b);
+                            break;
+                        }
+                    case "has":
+                        {
+                            list.ForEach(Console.WriteLine);
+                            
+                            break;
+                        }
+                    case "sort":
+                        { 
+                            
+                            list.Sort();
+                            foreach (int number in list)
+                            {
+                                Console.WriteLine(number);
+                            }                          
+                            break;
+                        }
+                    case "count":
+                        {
+                            int sum = 0;
+                            foreach (int count in list) 
+                            
+                            sum += count;
+                            Console.WriteLine(sum);
+                            
+                            break;
+                        }
+                    case "avg":
+                        {
+                            double a = 0;
+                            foreach (int avg in list)
+                                a += avg;
+                                a /= list.Count;
+                            Console.WriteLine(a);
+                            break;
+                        }
+                    case "max":
+                        {
+                            int a = list[0];
+                            foreach (int c in list)
+                            { if (c > a) 
+                                a = c;
+                            }      
+                            Console.WriteLine(a); 
+                            break;
+                        }
+                        case "min":
+                        {
+                            int a = list[0];
+                            foreach (int c in list)
+                            {
+                                if (c < a)
+                                    a = c;
+                            }
+                            Console.WriteLine(a);
+                            break;
+                        }
+                        case "get":
+                        {
+                            string a = Console.ReadLine();
+                            int b = int.Parse(a);
+                            bool c = list.Contains(b);
+                            Console.WriteLine(c);
+                            break;
+                        }
+                    case "help":
+                        {
+                            Console.WriteLine("Zoznam:");
+                            Console.WriteLine("add (pridat cislo)");
+                            Console.WriteLine("del (zmazat cislo)");
+                            Console.WriteLine("delat (zmazat konkretne cislo)");
+                            Console.WriteLine("has (vypisat zoznam)");
+                            Console.WriteLine("sort (vypise zoznam od najmensieho cisla)");
+                            Console.WriteLine("end (ukoncit program)");
+                            Console.WriteLine("count (napisat sucet vsetkych  cisiel)");
+                            Console.WriteLine("avg (napisat priemer cisiel)");
+                            Console.WriteLine("max (napisat najvecsie cislo)");
+                            Console.WriteLine("min (napisat najmensie cislo)");
+                            Console.WriteLine("get (napisat cislo na danom indexe)");
+                            Console.WriteLine("help (vypise vsetky prikazy)");
+                            Console.WriteLine("adds (pridanie viacej cisiel naraza le musi byt za nimi medzera)");
                             break;
                         }
                     case "end":
                         {
-                            break;
+                            return;
                         }
-                    default:
-                        {
-                            break;
-
-                        }
-
-                        //doplnit ostane veci cez case 
-
-
-
-
-
-
-
-
-
-
-
                 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             }
         }
     }
