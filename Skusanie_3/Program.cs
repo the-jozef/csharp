f07004fed8f7d83dc6d9d46084c4652b58853546
Jozef Lisko <jozeflisko0@gmail.com>
2025-10-24 21:47:06 +0200

    Random

---
+++ Skusanie_3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Skusanie_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random number = new Random();
+            int randomNumber = number.Next(1, 101);
+       
+            Console.WriteLine("Napíš mi nejaké číslo od 1 do 100");
+            
+            while (true)
+            {
+                Console.Write("Napis cislo: ");
+                int a = int.Parse(Console.ReadLine());
+
+                if (randomNumber > a) //20>15
+                {
+                    Console.WriteLine("Cislo je vacsie");
+                    
+
+                }
+                else if (randomNumber < a)
+                    {
+                    Console.WriteLine("Cislo je mensie");
+                }
+                else if (randomNumber == a)
+                        {
+                            Console.WriteLine("Uhadol si cislo,Gratulujem");
+                    break;
+
+                        }
+
+                    }
+            
+        }
+        
+    }
+}
