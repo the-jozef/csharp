36685b3bb5de9ae736a61dd1766e75526c098e11
Jozef Lisko <jozeflisko0@gmail.com>
2026-03-01 18:19:02 +0100

    website

--- Wpf_website/MainWindow.xaml.cs
+++ Wpf_website/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -19,11 +21,19 @@ namespace Wpf_website
         public MainWindow()
         {
             InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam); 
+        private void ControlBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 0xA1, 0x2, 0);
         }
 
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        private void ControlBar_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
         }
     }
 }
