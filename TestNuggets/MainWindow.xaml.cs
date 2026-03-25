fce9769037ceccc161d97d59feabeedf4ebe4eb6
Jozef Lisko <jozeflisko0@gmail.com>
2026-03-25 22:48:18 +0100

    nuggets

---
+++ TestNuggets/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TestNuggets.Viewmodels;
+
+namespace TestNuggets
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Toto je najdôležitejší riadok – pripojíme "mozog"
+            DataContext = new MainViewModel();
+        }
+        
+    }
+}
---
+++ TestNuggets/ViewModels.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+    using CommunityToolkit.Mvvm.Input;
+    using System.Windows;
+namespace TestNuggets.Viewmodels
+{
+
+
+
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _userName = "";
+
+        // NOVÉ: Počet znakov v mene (bude sa automaticky aktualizovať)
+        [ObservableProperty]
+        private int _nameLength = 0;
+        
+        [RelayCommand]
+        private void Greet()
+        {
+            if (string.IsNullOrWhiteSpace(UserName))
+            {
+                MessageBox.Show("Prosím zadaj svoje meno!", "Chyba");
+                return;
+            }
+
+            MessageBox.Show($"Ahoj, {UserName}!", "Pozdrav");
+        }
+
+        // NOVÝ command: Vymaže meno
+        [RelayCommand]
+        private void Clear()
+        {
+            UserName = "";
+            NameLength = 0;
+        }
+        [RelayCommand]
+        private void UpperCase()
+        {
+            UserName = UserName.ToUpper();
+        }
+
+        // Špeciálna metóda, ktorá sa zavolá automaticky keď sa zmení UserName
+        partial void OnUserNameChanged(string value)
+        {
+            NameLength = value?.Length ?? 0;
+        }
+    }
+}
+
